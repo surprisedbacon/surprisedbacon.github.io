a538fe6eafa939d73bb59a13f7e3f192aac35ab7
surprisedbacon <diane.a.demere@gmail.com>
2020-04-13 21:14:02 -0700

    colour transition and timer

--- index.js
+++ index.js
@@ -4,34 +4,56 @@ function domReady() {
 
 function toggleButton() {
   var button = document.getElementById('start');
+  var bodyStyle = document.getElementById('body').style;
   button.style.display = 'none';
   
   var demo = document.getElementById("demo");
-  demo.innerHTML = "3m 0s";
-  var distance = 5;
+  bodyStyle['background-color'] = '#E3493B'
+  demo.innerHTML = "3:00";
+  var distance = 16;
 
   // Update the count down every 1 second
   var x = setInterval(function() {
-      console.log(distance);
     // Find the distance between now and the count down date
     --distance;
+    console.log(distance);
       
     // Time calculations for minutes and seconds
     var minutes = Math.floor(distance / 60);
     var seconds = Math.floor(distance % 60);
       
     // Output the result in an element with id="demo"
-    demo.innerHTML = minutes + "m " + seconds + "s ";
+    if (seconds >= 10) {
+      demo.innerHTML = minutes + ":" + seconds;
+    // } else if (seconds < 1 && minutes < 0) {
+    //   debugger;  
+    } else {
+      demo.innerHTML = minutes + ":0" + seconds;
+    }
       
     // Colour change at certain times
-    if (distance === 135) {
-      
+    if (distance === 135 || distance === 75 || distance === 15) { //automate process: -0:45,then -1:00, -1:00, etc
+      bodyStyle['animation'] = 'redblue 1s';
     }
+
+    if (distance === 14 || distance === 134 || distance === 74) {
+      bodyStyle['animation'] = '';
+      bodyStyle['background-color'] = "#23B5AF";
+    }
+
+    if (distance === 119 || distance === 59) {
+      bodyStyle['background-color'] = '#E3493B'
+    }
+
+    if (distance === 120 || distance === 60) {
+      bodyStyle['animation'] = 'bluered 1s'
+    };
     // If the count down is over, write some text 
     if (distance === 0) {
       clearInterval(x);
       button.style.display = 'block';
       demo.innerHTML = "done!";
+      bodyStyle['background-color'] = '#A9DDD9'
     }
   }, 1000);
   
